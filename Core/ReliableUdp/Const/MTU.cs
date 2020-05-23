3df774ceaf5016719948d3fcfc6162ce6565b1c4
kdsbest <kdsbest@gmx.de>
2020-05-24 01:43:37 +0200

    Added 1200 and 1400 mtu. Fixed Tests

--- Core/ReliableUdp/Const/MTU.cs
+++ Core/ReliableUdp/Const/MTU.cs
@@ -1,10 +1,12 @@
 ﻿namespace ReliableUdp.Const
 {
-	public static class Mtu
-	{
-		public static readonly int[] PossibleValues =
-		{
-				576 - HeaderSize.MAX_UDP,  //Internet Path MTU for X.25 (RFC 879)
+    public static class Mtu
+    {
+        public static readonly int[] PossibleValues =
+        {
+            576 - HeaderSize.MAX_UDP,  //Internet Path MTU for X.25 (RFC 879)
+            1200 - HeaderSize.MAX_UDP, //Often used by consoles
+            1400 - HeaderSize.MAX_UDP, //Often used by consoles
             1492 - HeaderSize.MAX_UDP, //Ethernet with LLC and SNAP, PPPoE (RFC 1042)
             1500 - HeaderSize.MAX_UDP, //Ethernet II (RFC 1191)
             4352 - HeaderSize.MAX_UDP, //FDDI
@@ -12,6 +14,6 @@
             7981 - HeaderSize.MAX_UDP  //WLAN
         };
 
-		public static int MaxPacketSize = PossibleValues[PossibleValues.Length - 1];
-	}
+        public static int MaxPacketSize = PossibleValues[PossibleValues.Length - 1];
+    }
 }
